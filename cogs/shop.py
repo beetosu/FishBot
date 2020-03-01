e072ea4da075f1b026ee36be9b44b7a5fd4734af
beetosu <cpwalsh@buffalo.edu>
2020-03-01 14:20:00 -0500

    adding shop

---
+++ cogs/shop.py
@@ -0,0 +1,43 @@
+import discord
+from discord.ext import commands
+import sqlite3
+import data
+
+class Shop(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('shop is online')
+
+    @commands.command()
+    async def shop(self, ctx):
+        for rod, info in data.rods.items():
+            if info["shop"]:
+                await ctx.send(str(rod) +  "- $" + str(info["cost"]))
+
+    @commands.command()
+    async def buy(self, ctx, item, type):
+        product = item + " " + type
+        if product in data.rods.keys():
+            conn = sqlite3.connect('databases/players.db')
+            cur = conn.cursor()
+            player = cur.execute('SELECT * FROM users WHERE id="' + str(ctx.author.id) + "_" + str(ctx.guild.id) + '"')
+            for i in player:
+                if int(i[5]) < data.rods[product]["cost"]:
+                    await ctx.send("you do not have enough money for " + product)
+                    break
+                if i[1] == product:
+                    await ctx.send(product + " already bought!")
+                    break
+                newCoins = str(int(i[5]) - data.rods[product]["cost"])
+                cur.execute('UPDATE users SET money = ' +  newCoins + ', rod = "' + product + '" WHERE id ="' + str(ctx.author.id) + "_" + str(ctx.guild.id) + '"')
+                await ctx.send(product + " purchased!")
+            conn.commit()
+            conn.close()
+        else:
+            await ctx.send(product + " not found.")
+
+def setup(client):
+    client.add_cog(Shop(client))
--- data.py
+++ data.py
@@ -1,14 +1,43 @@
 ready_fish = {}
 
-fish_types = [[{"name": "Boot", "emoji": ":boot:", "points": 0, "money": 5, "odds": "common"},
-{"name": "Tuna", "emoji": ":fish:", "points": 5, "money": 10, "odds": "common"},
-{"name": "Yellow Tang", "emoji": ":tropical_fish:", "points": 15, "money": 15, "odds": "uncommon"},
-{"name": "Pufferfish", "emoji": ":blowfish:", "points": 35, "money": 20, "odds": "rare"}]]
+fish_types = [
+    [{"name": "Boot", "emoji": ":boot:", "points": 5, "money":0},
+    {"name": "Tuna", "emoji": ":fish:", "points": 5, "money": 10},
+    {"name": "Yellow Tang", "emoji": ":tropical_fish:", "points": 15, "money": 15},
+    {"name": "Pufferfish", "emoji": ":blowfish:", "points": 35, "money": 20}],
+
+    [{"name": "Super Boot", "emoji": ":high_heel:", "points": 10, "money": 0}]]
 
 rods = {
-    "Wooden Pole": {"desc": "Just the basics, nothing too fancy going on here.", "attraction": 0, "catch": 0, "degredation": 1, "in shop": False, "level": 0}
+    "Wooden Pole": {
+        "desc": "Just the basics, nothing too fancy going on here.",
+        "attraction": 1,
+        "catch": 1,
+        "shop": False,
+        "cost": 0
+        },
+    "Sturdy Pole": {
+        "desc": "Like the wooden pole, but a little hartier.\nMore forgiving catch rate",
+        "attraction": 1,
+        "catch": 2,
+        "shop": True,
+        "cost": 20
+        }
+}
+
+reel = {
+    1: {
+        "desc": "The standard amount, only reaches the surface of the sea",
+        "cost": 0,
+        "shop": False
+    },
+    2: {
+        "desc": "Twice as long as the standard reel, able to reach the twilight zone",
+        "cost": 100,
+        "shop": False
+    }
 }
 
 bait = []
 
-fish_odds = [[.385, .385, .192, .038]]
+fish_odds = [[.385, .385, .192, .038], [1]]
